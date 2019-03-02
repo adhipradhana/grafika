bed9bbc962c0a8e2c42c258033a42972eaeead2c
Nicholas R.P <nicholasmagbanua@gmail.com>
2019-03-02 18:42:15 +0700

    add line, circle, and poly parser

--- image/draw.py
+++ image/draw.py
@@ -1,7 +1,7 @@
 import sys
 
 if (len(sys.argv) != 3):
-    print('Usage: python draw.py <filename>')
+    print('Usage: python draw.py <filename> <type>')
     print("""
       Type:
       1 Line
@@ -16,16 +16,36 @@ width = 25
 file = open(filename + '.txt', "r")
 new_file = open(filename + '_large.txt', "w")
 
-for line in file:
-	x1, y1, x2, y2, color = map(int, line.split(','))
-	for i in range(width):
-		x1_new = 1000 - (x1 + i)
-		y1_new = 1000 - (y1 + i)
-		x2_new = 1000 - (x2 + i)
-		y2_new = 1000 - (y2 + i)
+if (sys.argv[2] == "1"):
+  for line in file:
+    x1, y1, x2, y2, color = map(int, line.split(','))
+    for i in range(width):
+      x1_new = 1000 - (x1 + i)
+      y1_new = 1000 - (y1 + i)
+      x2_new = 1000 - (x2 + i)
+      y2_new = 1000 - (y2 + i)
 
-		new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(x2_new) + ',' + str(y2_new) + ',' + color + '\n'
-		new_file.writelines(new_line)
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(x2_new) + ',' + str(y2_new) + ',' + color + '\n'
+      new_file.writelines(new_line)
+elif (sys.argv[2] == "2"):
+  for circle in file:
+    x1, y1, r, color = map(int, circle.split(','))
+    for i in range(width):
+      x1_new = 1000 - (x1 + i)
+      y1_new = 1000 - (y1 + i)
 
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(r) + ',' + color + '\n'
+      new_file.writelines(new_line)
+elif (sys.argv[3] == "3"):
+  for line in file:
+    if (',' in line):
+      x1, y1, color = map(int, line.split(','))
+      x1_new = 1000 - (x1 + i)
+      y1_new = 1000 - (y1 + i)
+
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + color + '\n'
+      new_file.writelines(new_line)
+    else:
+      new_file.writelines(line)
 file.close()
 new_file.close()
