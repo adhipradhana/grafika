afc70079e4a6c55ee3ad851bd132c6cb89c38999
Nicholas R.P <nicholasmagbanua@gmail.com>
2019-03-02 18:50:49 +0700

    script

--- image/draw.py
+++ image/draw.py
@@ -18,34 +18,37 @@ new_file = open(filename + '_large.txt', "w")
 
 if (sys.argv[2] == "1"):
   for line in file:
-    x1, y1, x2, y2, color = map(int, line.split(','))
+    x1, y1, x2, y2, color = line.split(',')
     for i in range(width):
-      x1_new = 1000 - (x1 + i)
-      y1_new = 1000 - (y1 + i)
-      x2_new = 1000 - (x2 + i)
-      y2_new = 1000 - (y2 + i)
+      x1_new = 1000 - (float(x1) + i)
+      y1_new = 1000 - (float(y1) + i)
+      x2_new = 1000 - (float(x2) + i)
+      y2_new = 1000 - (float(y2) + i)
 
-      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(x2_new) + ',' + str(y2_new) + ',' + color + '\n'
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(x2_new) + ',' + str(y2_new) + ',' + color.split("\n")[0] + '\n'
       new_file.writelines(new_line)
 elif (sys.argv[2] == "2"):
   for circle in file:
-    x1, y1, r, color = map(int, circle.split(','))
+    x1, y1, r, color = circle.split(',')
     for i in range(width):
-      x1_new = 1000 - (x1 + i)
-      y1_new = 1000 - (y1 + i)
+      x1_new = 1000 - (float(x1) + i)
+      y1_new = 1000 - (float(y1) + i)
 
-      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(r) + ',' + color + '\n'
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + str(r) + ',' + color.split("\n")[0] + '\n'
       new_file.writelines(new_line)
-elif (sys.argv[3] == "3"):
+elif (sys.argv[2] == "3"):
+  i = 0
   for line in file:
     if (',' in line):
-      x1, y1, color = map(int, line.split(','))
-      x1_new = 1000 - (x1 + i)
-      y1_new = 1000 - (y1 + i)
+      x1, y1, color = line.split(',')
+      x1_new = 1000 - (float(x1) + i)
+      y1_new = 1000 - (float(y1) + i)
 
-      new_line = str(x1_new) + ',' + str(y1_new) + ',' + color + '\n'
+      new_line = str(x1_new) + ',' + str(y1_new) + ',' + color.split("\n")[0] + '\n'
       new_file.writelines(new_line)
+      i+=1
     else:
       new_file.writelines(line)
+      i=0
 file.close()
 new_file.close()
